5f3d10429e12299a7ceb976741e8e96b7383aff9
Siddharth Sinha <siddharthsinha9752@gmail.com>
2021-02-20 01:05:22 +0530

    other stuff

--- src/Components/Dashboard/Search/Search.jsx
+++ src/Components/Dashboard/Search/Search.jsx
@@ -1,16 +1,32 @@
-import { Grid } from '@material-ui/core'
+import { Avatar, Box, Divider, Grid, Typography } from '@material-ui/core'
 import React, { Component } from 'react'
 import Powered from './powered'
 
 import algoliasearch from 'algoliasearch/lite';
-import { SearchBox, InstantSearch, Hits } from 'react-instantsearch-dom'
-import 'instantsearch.css/themes/satellite-min.css';
+import { SearchBox, InstantSearch, Hits, Highlight, Configure, Index } from 'react-instantsearch-dom'
 import './search.css'
+import 'instantsearch.css/themes/satellite-min.css';
+
 
 export default class Search extends Component {
 
     render() {
-        const Hit = ({ hit }) => (<p>{hit.name}</p>)
+        const HitP = ({ hit }) => {
+            return (
+                <Grid container alignItems="center" justify="space-between">
+                    <Avatar src={hit.profilePic}>{hit.name}</Avatar><Highlight hit={hit} attribute="name" />
+                    <Highlight hit={hit} attribute="mobile_no" />
+                </Grid>
+            )
+        }
+        const HitE = ({ hit }) => {
+            return (
+                <Grid container alignItems="center" justify="space-between">
+                    <Avatar src={hit.profilePic}>{hit.name}</Avatar><Highlight hit={hit} attribute="name" />
+                    <Highlight hit={hit} attribute="jobTitle" />
+                </Grid>
+            )
+        }
         const searchClient = algoliasearch(
             'PE7TK68BSA',
 
@@ -18,14 +34,39 @@ export default class Search extends Component {
         );
         return (
             <Grid item sm={11} xs={12} style={{ padding: 10 }}>
-                <Grid container direction="row" justify="flex-e">
+                <Grid style={{ margin: 20 }}>
+                    <InstantSearch indexName="prod_orgPatients" searchClient={searchClient} stalledSearchDelay={500}>
+                        <Grid container direction="row" style={{ width: "100%" }}>
+                            <Grid item sm={3}>
+                                <Powered />
+                            </Grid>
+                            <Grid item sm={9}>
+                                <Grid container direction="column" justify="flex-end" style={{ height: "100%" }}>
+                                    <SearchBox showLoadingIndicator searchAsYouType={false} />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Configure hitsPerPage={3} />
+                        <Divider variant="fullWidth" style={{ marginTop: 30, marginBottom: 20 }} />
+                        <Grid container>
+                            <Grid item sm={6} xs={12} style={{ padding: 10 }}>
+                                <Typography color="textSecondary"><Box fontWeight="fontWeightLight" padding="10px">Patient Results</Box></Typography>
+                                <Grid container direction="column" style={{ margin: 10, background: "#181818", borderRadius: "10px" }}>
+                                    <Index indexName="prod_orgPatients">
 
-                    <Powered />
-                </Grid>
-                <Grid style={{ marginTop: 20 }}>
-                    <InstantSearch indexName="prod_orgPatients" searchClient={searchClient}>
-                        <SearchBox showLoadingIndicator />
-                        <Hits hitComponent={Hit} />
+                                        <Hits hitComponent={HitP} />
+                                    </Index>
+                                </Grid>
+                            </Grid>
+                            <Grid item sm={6} xs={12} style={{ padding: 10 }}>
+                                <Typography color="textSecondary"><Box fontWeight="fontWeightLight" padding="10px">Employee Results</Box></Typography>
+                                <Grid container direction="column" style={{ margin: 10, background: "#181818", borderRadius: "10px" }}>
+                                    <Index indexName="prod_orgMembers">
+                                        <Hits hitComponent={HitE} />
+                                    </Index>
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </InstantSearch>
                 </Grid>
             </Grid>
