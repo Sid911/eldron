c170cc0657572f99380164bd9e0afb60292e54ee
Siddharth Sinha <siddharthsinha9752@gmail.com>
2021-02-19 16:53:12 +0530

    added bar graph template

--- src/Components/Dashboard/Search/Search.jsx
+++ src/Components/Dashboard/Search/Search.jsx
@@ -2,13 +2,28 @@ import { Grid } from '@material-ui/core'
 import React, { Component } from 'react'
 import Powered from './powered'
 
+import algoliasearch from 'algoliasearch/lite';
+import { SearchBox, InstantSearch } from 'react-instantsearch-dom'
+import 'instantsearch.css/themes/satellite-min.css';
+import './search.css'
+
 export default class Search extends Component {
+
     render() {
+        const searchClient = algoliasearch(
+            'PE7TK68BSA',
+
+            'f5d515ac9cd37d8022c14bb0ce3cfedc'
+        );
         return (
             <Grid item sm={11} xs={12} style={{ padding: 10 }}>
                 <Grid container direction="row" justify="space-between">
                     <Powered />
-
+                </Grid>
+                <Grid style={{ marginTop: 20 }}>
+                    <InstantSearch indexName="prod_orgPatients" searchClient={searchClient}>
+                        <SearchBox showLoadingIndicator searchAsYouType={false} />
+                    </InstantSearch>
                 </Grid>
             </Grid>
         )
--- src/Components/Dashboard/Stats/Graphs/AppointmentGraphs.jsx
+++ src/Components/Dashboard/Stats/Graphs/AppointmentGraphs.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {
+    XYPlot,
+    XAxis,
+    YAxis,
+    VerticalGridLines,
+    HorizontalGridLines,
+    VerticalBarSeries,
+} from 'react-vis'
 
 function AppointmentGraphs() {
+    const greenData = [{ x: 'A', y: 10 }, { x: 'B', y: 5 }, { x: 'C', y: 15 }];
+
+    const blueData = [{ x: 'A', y: 12 }, { x: 'B', y: 2 }, { x: 'C', y: 11 }];
     return (
         <div>
-            Item Two
+            <XYPlot xType="ordinal" width={500} height={300} xDistance={100}>
+                <VerticalGridLines />
+                <HorizontalGridLines />
+                <XAxis />
+                <YAxis />
+                <VerticalBarSeries className="vertical-bar-series-example" data={greenData} />
+                <VerticalBarSeries data={blueData} />
+            </XYPlot>
         </div>
     )
 }
