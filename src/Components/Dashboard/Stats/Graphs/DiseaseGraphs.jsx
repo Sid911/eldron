f70b30f6b2c2e3fcfd9fcd373e40f6a8336b115e
Goku-pr <priyanshusahu91@gmail.com>
2021-02-19 16:34:21 +0530

    Added dummy data for desiase graph

--- src/Components/Dashboard/Stats/Graphs/DiseaseGraphs.jsx
+++ src/Components/Dashboard/Stats/Graphs/DiseaseGraphs.jsx
@@ -14,13 +14,10 @@ class DiseaseGraphs extends Component {
 
     render() {
         const ITEMS = [
-            'Options',
-            'Buttons',
-            'Select boxes',
-            'Date inputs',
-            'Password inputs',
-            'Forms',
-            'Other'
+            'Covid-19',
+            'Bone fracture',
+            'Viral fever',
+            'Heart Deasise',
         ];
         return (
             <Grid container style={{ flexGrow: 1 }}>
@@ -32,10 +29,16 @@ class DiseaseGraphs extends Component {
                         <YAxis style={{ text: { stroke: 'none', fill: '#ffff', fontSize: 12 } }} />
                         <LineSeries
 
-                            data={[{ x: 1, y: 10 }, { x: 2, y: 5 }, { x: 3, y: 15 }]}
+                            data={[{ x:1 , y:1  }, { x:2 , y:1.3  }, { x:3 , y:1.5  }, { x:4 , y:3  }, { x:5 , y:3.5  }, { x:6 , y:4  }, { x:7 , y:6  }, { x:8 , y:8 }, { x:9 , y:10  }, { x:10 , y:9  }, { x:11 , y:7  }, { x:12 , y:6  }]}
                         />
                         <LineSeries
-                            data={[{ x: 1, y: 6 }, { x: 2, y: 7 }, { x: 3, y: 12 }]}
+                            data={[{ x:1 , y:0.6  }, { x:2 , y:0.7  }, { x:3 , y:0.3  }, { x:4 , y:0.4  }, { x:5 , y:0.5  }, { x:6 , y:0.4  }, { x:7 , y:0.8  }, { x:8 , y:1.2 }, { x:9 , y:1  }, { x:10 , y:0.2 }, { x:11 , y:0.5  }, { x:12 , y:0.6  }]}
+                        />
+                        <LineSeries
+                            data={[{ x:1 , y:2  }, { x:2 , y:3  }, { x:3 , y:2.5  }, { x:4 , y:3.5  }, { x:5 , y:4  }, { x:6 , y:4  }, { x:7 , y:3.6  }, { x:8 , y:2.7 }, { x:9 , y:2 }, { x:10 , y:2.3 }, { x:11 , y:2  }, { x:12 , y:1.8 }]}
+                        />
+                        <LineSeries
+                            data={[{ x:1 , y:4  }, { x:2 , y:5  }, { x:3 , y:5.5  }, { x:4 , y:4.7  }, { x:5 , y:4.4  }, { x:6 , y:4.3  }, { x:7 , y:4.7  }, { x:8 , y:5.2 }, { x:9 , y:5.5  }, { x:10 , y:4.9 }, { x:11 , y:4.5  }, { x:12 , y:4.7  }]}
                         />
                     </XYPlot>
                 </Grid>
