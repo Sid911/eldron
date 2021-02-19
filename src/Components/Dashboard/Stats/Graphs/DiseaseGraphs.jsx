8acac8df8272bc46084cd13cd0772b882d152895
Siddharth Sinha <siddharthsinha9752@gmail.com>
2021-02-19 15:31:40 +0530

    removed stuff

--- src/Components/Dashboard/Stats/Graphs/DiseaseGraphs.jsx
+++ src/Components/Dashboard/Stats/Graphs/DiseaseGraphs.jsx
@@ -5,29 +5,13 @@ import {
     XAxis,
     YAxis,
     LineSeries,
-    Hint,
     HorizontalGridLines,
     VerticalGridLines,
     DiscreteColorLegend
 } from 'react-vis'
 
 class DiseaseGraphs extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        };
-    }
 
-    _forgetValue = () => {
-        this.setState({
-            value: null
-        });
-    };
-
-    _rememberValue = value => {
-        this.setState({ value });
-    };
     render() {
         const ITEMS = [
             'Options',
@@ -38,7 +22,6 @@ class DiseaseGraphs extends Component {
             'Forms',
             'Other'
         ];
-        const { value } = this.state;
         return (
             <Grid container style={{ flexGrow: 1 }}>
                 <Grid item md={8} sm={12}>
@@ -48,16 +31,12 @@ class DiseaseGraphs extends Component {
                         <XAxis style={{ text: { stroke: 'none', fill: '#ffff', fontSize: 12 } }} />
                         <YAxis style={{ text: { stroke: 'none', fill: '#ffff', fontSize: 12 } }} />
                         <LineSeries
-                            onValueMouseOver={this._rememberValue}
-                            onValueMouseOut={this._forgetValue}
+
                             data={[{ x: 1, y: 10 }, { x: 2, y: 5 }, { x: 3, y: 15 }]}
                         />
                         <LineSeries
-                            onValueMouseOver={this._rememberValue}
-                            onValueMouseOut={this._forgetValue}
                             data={[{ x: 1, y: 6 }, { x: 2, y: 7 }, { x: 3, y: 12 }]}
                         />
-                        {value ? <Hint value={value} /> : null}
                     </XYPlot>
                 </Grid>
                 <Grid item md={4} sm={12} >
