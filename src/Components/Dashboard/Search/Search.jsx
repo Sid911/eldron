23d9f1e267a72def81c862e92a2046a0e449e185
Siddharth Sinha <siddharthsinha9752@gmail.com>
2021-02-20 01:04:44 +0530

    completed Algolia search

--- src/Components/Dashboard/Search/Search.jsx
+++ src/Components/Dashboard/Search/Search.jsx
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 import Powered from './powered'
 
 import algoliasearch from 'algoliasearch/lite';
-import { SearchBox, InstantSearch } from 'react-instantsearch-dom'
+import { SearchBox, InstantSearch, Hits } from 'react-instantsearch-dom'
 import 'instantsearch.css/themes/satellite-min.css';
 import './search.css'
 
 export default class Search extends Component {
 
     render() {
+        const Hit = ({ hit }) => (<p>{hit.name}</p>)
         const searchClient = algoliasearch(
             'PE7TK68BSA',
 
@@ -17,12 +18,14 @@ export default class Search extends Component {
         );
         return (
             <Grid item sm={11} xs={12} style={{ padding: 10 }}>
-                <Grid container direction="row" justify="space-between">
+                <Grid container direction="row" justify="flex-e">
+
                     <Powered />
                 </Grid>
                 <Grid style={{ marginTop: 20 }}>
                     <InstantSearch indexName="prod_orgPatients" searchClient={searchClient}>
-                        <SearchBox showLoadingIndicator searchAsYouType={false} />
+                        <SearchBox showLoadingIndicator />
+                        <Hits hitComponent={Hit} />
                     </InstantSearch>
                 </Grid>
             </Grid>
